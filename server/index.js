3016d792a7009746be3459d850273043fac72a2e
SonderUA <umarbek.sonder@gmail.com>
2025-01-20 21:42:44 +0500

    feat(server): fix a bug and add POST route for form submission

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 
+const app = express();
+
 // Middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const app = express();
+app.post("/form", (req, res) => {
+	const { role, range, country, agreement } = req.body;
 
-app.get("/", (req, res) => {
-	return res.status(200).json({ message: "Hello World!" });
+	if (!role || !range || !country || !agreement) {
+		return res.status(400).json({ message: "All fields are required!" });
+	}
+
+	console.log({ role, range, country, agreement });
+
+	return res.status(200).json({ message: "Form submitted successfully!" });
 });
 
 const PORT = 7070; // Change later

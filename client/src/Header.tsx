d2cf85dee7de0396a239a9fc0980ea1c8650ab82
SonderUA <umarbek.sonder@gmail.com>
2025-01-20 19:42:42 +0500

    feat(Header): add layout with styling and CicularProgressBar.tsx to Header.tsx

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,5 +1,19 @@
+import React from "react";
+import { TfiAlignJustify } from "react-icons/tfi";
+import CircularProgressBar from "./CircularProgressBar";
+
 const Header: React.FC = () => {
-	return <header></header>;
+	return (
+		<header className="flex p-8 justify-around items-center">
+			<div>
+				<a href="/" className="flex items-center gap-2">
+					<TfiAlignJustify className="text-2xl" />
+					<h1 className="text-3xl font-bold">Baserow</h1>
+				</a>
+			</div>
+			<CircularProgressBar />
+		</header>
+	);
 };
 
 export default Header;
